d081053e7d06fabde1b37f3b85f88da9d48d54bc
Wesley Denaro <Wesley.Denaro@iCloud.com>
2018-03-19 14:16:34 -0400

    Basic form validation and exercise invilving ngForm, ngModel and ngSubmit. Initial Commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { CourseFormComponent } from './course-form/course-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/course-form/course-form.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-form',
+  templateUrl: './course-form.component.html',
+  styleUrls: ['./course-form.component.scss']
+})
+export class CourseFormComponent {
+  categories = [
+    { id: 1, name: 'Development' },
+    { id: 2, name: 'Art' },
+    { id: 3, name: 'Languages' }
+  ];
+
+  onSubmit(f) {
+    console.log(f);
+  }
+}
